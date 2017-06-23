1afec389ac0b5ff638d099c2afc791ebf3def289
Park.hyomin <miw_hadis@msn.com>
2017-06-24 03:41:27 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+#required vars
+CONSUL_ROOT=/consul
+CONFIG_FILE=${HAPROXY_ROOT}/haproxy.cfg
+TEMPLATE=/etc/consul-template/tmpl.json
+
+HAPROXY_STATUS_PORT=${HAPROXY_STATUS_PORT:-9000}
+HAPROXY_PROXY_PORT=${HAPROXY_PROXY_PORT:-80}
+HAPROXY_DOMAIN=${HAPROXY_DOMIAN:-rongyi.com}
+CONSUL_SERVER=${CONSUL_SERVER:-127.0.0.1}
+CONSUL_PORT=${CONSUL_PORT:-8500}
+LOG_LEVEL=${LOG_LEVEL:-debug}
+
+#start haproxy
+#systemctl start haproxy 
+#/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /run/haproxy.pid
+#/usr/sbin/haproxy -D -p /var/run/haproxy.pid  -f /etc/haproxy/haproxy.cfg -sf $(cat /var/run/haproxy.pid) || true
+#start consul-template as follow
+sed -e "s/HAPROXY_PROXY_PORT/${HAPROXY_PROXY_PORT}/g;s/HAPROXY_STATUS_PORT/${HAPROXY_STATUS_PORT}/g" /etc/consul-haproxy/haproxy.ctmpl.bak > /etc/consul-haproxy/haproxy.ctmpl
+/usr/local/bin/consul-template -consul $CONSUL_SERVER:$CONSUL_PORT \
+    -config $TEMPLATE \
+    -wait 2s:10s \
+    -log-level "$LOG_LEVEL" \
+    -syslog >/dev/null 2&1> &
