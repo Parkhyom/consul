bd92c8d16f5e70194e8a557496b7c1f3fc12564e
Park.hyomin <miw_hadis@msn.com>
2017-06-25 00:33:31 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/sbin/haproxy -D -p /var/run/haproxy.pid -f /etc/haproxy/haproxy.cfg -sf $(cat /var/run/haproxy.pid) || true
+/usr/local/sbin/haproxy -D -p /run/haproxy.pid -f /etc/haproxy/haproxy.cfg -sf $(cat /run/haproxy.pid) || true
